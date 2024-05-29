49cfec5a143c99616a8bf1bc1085a563cd60843d
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-05-29 15:30:42 +0530

    Save hparams and use name=main

--- autoencoder.py
+++ autoencoder.py
@@ -27,47 +27,48 @@ d_model = 128
 d_latent = 128
 sae = AutoEncoder(d_model, d_latent).to('mps')
 
-run_name = 'grok_1716823448'
-layer = 'embeddings'
-ckpt = 'final'
-data = torch.load(f'activations/{run_name}/{ckpt}_{layer}.pth', map_location='mps')
-
-α = 1e-5
-opt = optim.Adam(sae.parameters(), lr=1e-3)
-stopping_thresh = 0.0001
-
-root = Path('sae')
-(root/run_name).mkdir(parents=True,exist_ok=True)
-
-losses = []
-
-for epoch in range(10000):
-    output, latent = sae(data)
-    loss = F.mse_loss(output, data) + α * (1/d_latent) * latent.norm()
-
-    losses.append(loss.item())
-
-    if epoch%100 == 0: print(f"{epoch}_{loss.item():.4f}")
-
-    loss.backward()
-    opt.step()
-    opt.zero_grad()
-
-    if loss.item() < stopping_thresh:
-        break
-
-save_dict = {
-    'model': sae.state_dict(),
-    'optimizer': opt.state_dict(),
-    'alpha': α,
-    'loss': loss,
-    'losses': losses,
-    'epoch': epoch,
-}
-
-torch.save(save_dict, root/run_name/f"{ckpt}_{layer}.pth")
-print(f"Saved model to {root/run_name/f'{ckpt}_{layer}.pth'}")
-
-plt.plot(losses)
-plt.yscale('log')
-plt.show()
+if __name__ == '__main__':
+    run_name = 'grok_1716823448'
+    layer = 'embeddings'
+    ckpt = 'final'
+    data = torch.load(f'activations/{run_name}/{ckpt}_{layer}.pth', map_location='mps')
+    
+    α = 1e-5
+    opt = optim.Adam(sae.parameters(), lr=1e-3)
+    stopping_thresh = 0.0001
+    
+    root = Path('sae')
+    (root/run_name).mkdir(parents=True,exist_ok=True)
+    
+    losses = []
+    
+    for epoch in range(10000):
+        output, latent = sae(data)
+        loss = F.mse_loss(output, data) + α * (1/d_latent) * latent.norm()
+    
+        losses.append(loss.item())
+    
+        if epoch%100 == 0: print(f"{epoch}_{loss.item():.4f}")
+    
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+    
+        if loss.item() < stopping_thresh:
+            break
+        
+    save_dict = {
+        'model': sae.state_dict(),
+        'optimizer': opt.state_dict(),
+        'alpha': α,
+        'loss': loss,
+        'losses': losses,
+        'epoch': epoch,
+    }
+    
+    torch.save(save_dict, root/run_name/f"{ckpt}_{layer}_{d_latent}_{α}.pth")
+    print(f"Saved model to {root/run_name/f'{ckpt}_{layer}_{d_latent}_{α}.pth'}")
+    
+    plt.plot(losses)
+    plt.yscale('log')
+    plt.show()
